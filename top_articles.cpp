623d44b084049f0632a15c6d2c9cb91c48470a20
Josephat-Onkoba <josephatonkoba30@gmail.com>
2026-05-20 00:14:14 +0300

    annex assessment

--- top_articles.cpp
+++ top_articles.cpp
@@ -16,9 +16,7 @@ struct Article {
 size_t WriteCallback(void* contents, size_t size, size_t nmemb, string* output) {
 
     size_t totalSize = size * nmemb;
-
     output->append((char*)contents, totalSize);
-
     return totalSize;
 }
 
@@ -26,21 +24,15 @@ string fetchAPI(string url) {
 
     CURL* curl;
     CURLcode res;
-
     string response;
-
     curl = curl_easy_init();
 
     if (curl) {
 
         curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
-
         res = curl_easy_perform(curl);
-
         curl_easy_cleanup(curl);
     }
 
@@ -52,7 +44,6 @@ bool compareArticles(const Article& a, const Article& b) {
     if (a.num_comments != b.num_comments) {
         return a.num_comments > b.num_comments;
     }
-
     return a.name > b.name;
 }
 
@@ -61,7 +52,6 @@ vector<string> topArticles(int limit) {
     vector<Article> all_articles;
 
     int total_pages = 1;
-
     int current_page = 1;
 
     while (current_page <= total_pages) {
@@ -71,9 +61,7 @@ vector<string> topArticles(int limit) {
             + to_string(current_page);
 
         string response = fetchAPI(url);
-
         json j = json::parse(response);
-
         total_pages = j["total_pages"];
 
         for (auto& item : j["data"]) {
@@ -81,17 +69,11 @@ vector<string> topArticles(int limit) {
             string final_name = "";
 
             if (!item["title"].is_null()) {
-
                 final_name = item["title"];
-
             }
-
             else if (!item["story_title"].is_null()) {
-
                 final_name = item["story_title"];
-
             }
-
             else {
                 continue;
             }
@@ -106,7 +88,6 @@ vector<string> topArticles(int limit) {
 
             article.name = final_name;
             article.num_comments = comments;
-
             all_articles.push_back(article);
         }
 
@@ -120,7 +101,6 @@ vector<string> topArticles(int limit) {
     int items_to_get = min(limit, (int)all_articles.size());
 
     for (int i = 0; i < items_to_get; i++) {
-
         result.push_back(all_articles[i].name);
     }
 
